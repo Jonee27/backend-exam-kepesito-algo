806318986c413c90a8e18aaf47c657ac1356d4bc
Jonee27 <jonee271@gmail.com>
2021-04-29 10:06:34 +0200

    Done!?!?

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,16 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+    public int addDigits(String input) {
+        int sum = 0;
+        for (char character: input.toCharArray()){
+            try {
+               int number = Integer.parseInt(String.valueOf(character));
+               sum += number;
+            } catch (NumberFormatException nfe){
+                nfe.printStackTrace();
+            }
+        }
+        return sum;
+    }
 }
