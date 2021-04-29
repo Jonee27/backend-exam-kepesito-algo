386c3c753afb8e6ce77127243bf58e6471db6a1a
Jonee27 <jonee271@gmail.com>
2021-04-29 11:16:54 +0200

    minor refactor :D

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -3,16 +3,17 @@ package hu.nive.ujratervezes.adddigits;
 public class AddDigits {
 
     public int addDigits(String input) {
-        int sum = 0;
-        if (input != null && !input.isEmpty()) {
-            for (char character : input.toCharArray()) {
-                if (Character.isDigit(character)) {
-                    sum += Character.getNumericValue(character);
-                }
-            }
-        } else {
+        if(input == null || input.isEmpty()){
             return -1;
         }
+
+        int sum = 0;
+        for (char character : input.toCharArray()) {
+            if (Character.isDigit(character)) {
+                sum += Character.getNumericValue(character);
+            }
+        }
+
         return sum;
     }
 }
