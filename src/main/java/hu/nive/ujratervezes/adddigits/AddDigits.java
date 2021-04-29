ee0c204ba7cb8ca83f3a355df5db3aa19b451c95
Jonee27 <jonee271@gmail.com>
2021-04-29 10:49:41 +0200

    return -1...

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -3,12 +3,14 @@ package hu.nive.ujratervezes.adddigits;
 public class AddDigits {
     public int addDigits(String input) {
         int sum = 0;
-        if (input != null || !input.isEmpty()){
+        if (input != null && !input.isEmpty()){
             for (char character: input.toCharArray()){
                 if (Character.isDigit(character)){
                     sum += Character.getNumericValue(character);
                 }
             }
+        } else {
+            return -1;
         }
         return sum;
     }
